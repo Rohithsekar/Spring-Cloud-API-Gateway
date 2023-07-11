2eb4c92e67e0a2247ce4ad85462a2cea41fd9f60
HP <itsrohithsekar@gmail.com>
2023-07-11 19:24:17 +0530

    Added comment

--- src/main/java/rohi/gateway/api_gateway/FallbackController.java
+++ src/main/java/rohi/gateway/api_gateway/FallbackController.java
@@ -4,6 +4,25 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+Both spring-cloud-starter-circuitbreaker-reactor-resilience4j and spring-cloud-starter-netflix-hystrix dependencies need
+to be present in your project in order to achieve circuit-breaker and fallback meachanism.
+
+The reason for this is the conditional auto-configuration performed by Spring. The
+GatewayCircuitBreakerAutoConfiguration class, which is responsible for configuring the Spring Cloud CircuitBreaker
+filter, uses the @ConditionalOnClass annotation to check for the presence of certain classes. In this case, it checks
+for the presence of DispatcherHandler (a class from Spring WebFlux),
+ReactiveResilience4JAutoConfiguration (from spring-cloud-starter-circuitbreaker-reactor-resilience4j), and
+HystrixCircuitBreakerAutoConfiguration (from spring-cloud-starter-netflix-hystrix).
+
+Since the @ConditionalOnClass annotation specifies an "and" relationship between the classes, both dependencies need to
+be present for the auto-configuration to take place. Including both dependencies in your pom.xml file ensures that the
+required classes are available for the auto-configuration to work properly.
+
+To summarize, adding both spring-cloud-starter-circuitbreaker-reactor-resilience4j and
+spring-cloud-starter-netflix-hystrix dependencies to your project resolves the issue by providing the necessary classes
+for the auto-configuration of the Spring Cloud CircuitBreaker filter.
+ */
 
 @RestController
 public class FallbackController {
